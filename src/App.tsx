b362389bdf1df55188c65672510c545d9a3f35d6
Daniel <daniel.nel@typedev.co.za>
2024-11-16 12:41:04 +0200

    react router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./App.scss";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-      <Home />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<div>hello</div>} />
+      </Routes>
+    </Router>
   );
 }
 
